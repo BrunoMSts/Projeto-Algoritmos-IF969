8f7553f8bcd5caf54176f9c98cfc9a41376678d0
BrunoMSts <bms3@cin.ufpe.br>
2019-11-20 16:30:21 -0300

    alterações básicas

--- Controle.py
+++ Controle.py
@@ -1,2 +1,23 @@
+from Candidatos import *
+from Bens import *
+from Lista import *
+from leitor_CSV import *
+
 class Controle:
-    def __init__(self)
+    def __init__(self):
+        self.listinha = []
+
+    def carregamentoCandidatos(self, pathCandidatos):
+        path = pathCandidatos
+        arquivo = get_archives(path)
+        with open(arquivo[0], 'r', errors='ignore') as path:
+            for lines in path:
+                if 'DT_GERACAO' in lines:
+                    continue
+                lines = (lines.replace('"', '').strip()).split(';')
+                print(len(lines))
+                pessoa = Candidato(lines)
+                self.listinha.append(pessoa)
+        return self.listinha
+    def carregamentoBens(self, pathBens):
+        pass
--- leitor_CSV.py
+++ leitor_CSV.py
@@ -1,7 +1,8 @@
 import os
 
-diretorio_bem_candidatos= r'C:\Users\HOME\Desktop\Programação\Projeto Algoritmos\bem_candidato_2014'
-diretorio_consulta_cand = r'C:\Users\HOME\Desktop\Programação\Projeto Algoritmos\consulta_cand_2014'
+diretorio_bem_candidatos= r'/home/bruno/Área de Trabalho/Python/Projeto_Algoritmos/bem_candidato_2014' #COLOQUE O CAMINHO DO ARQUIVO AQUI
+diretorio_consulta_cand = r'' #COLOQUE O CAMINHO DO ARQUIVO AQUI
+
 datas = {'AC':{}, 'AL':{}, 'AM':{}, 'AP':{}, 'BA':{},
                'BR':{}, 'BRASIL':{}, 'CE':{}, 'DF':{}, 'ES':{},
                'GO':{}, 'MA':{}, 'MG':{}, 'MS':{}, 'MT':{}, 'PA':{},
@@ -15,7 +16,7 @@ def get_archives(directory):
     return archives[:len(archives)-1]
 
 def get_lines(archive):
-    a = open(archive, 'r')
+    a = open(archive, 'r', errors='ignore')
     lines = [x for x in range(len(a.readlines()))]
     a.close()
     return lines
@@ -29,7 +30,7 @@ def tratarStr(lista):
         
 def read_archives():
     archives = get_archives(diretorio_bem_candidatos)
-    colunms = open(archives[0], 'r').readline().split(';')
+    colunms = open(archives[0], 'r', errors='ignore').readline().split(';')
     datasAux = {}
     cont = 0
     for a in datas.keys():
@@ -43,7 +44,7 @@ def read_archives():
     for i in range(len(archives)): #IMPLEMENTAR O CÓDIGO QUE COLOCA OS VALORES EM CADA COLUNA DE CADA ESTADO
         lines = get_lines(archives[i])
         for j in lines:
-            b = open(archives[i], 'r')
+            b = open(archives[i], 'r', errors='ignore')
             arrayData = b.readlines()[j].split(';')
             arrayData = tratarStr(arrayData)
             break
